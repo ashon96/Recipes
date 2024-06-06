adbd4ac5b2521e786466f43e6b16e7ab047336ef
Andrew <andrew.shon@willowtreeapps.com>
2024-06-06 18:05:25 -0400

    Add initial code generation

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Andrew Shon on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
